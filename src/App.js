464a47310adbc9683bb0801ffe731e02530d0b6c
shrutijain2296 <shrutijain2296@gmail.com>
2023-07-29 23:47:04 +0530

    changed commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App =() =>{
   return(
     <div>
       <Routes>
-        <Route path = "/login" element = {<Login />} />
+        <Route path = "/" element = {<Login />} />
         <Route path = "/profile" element = {<Profile />} />
       </Routes>
       
